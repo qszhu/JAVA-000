597b9f58a6740a79a662ce7eff011e33363455fa
qszhu <qszhu2006@gmail.com>
2020-12-16 17:03:26 +0800

    copy rpc code

---
+++ Week_09/rpc01/rpcfx-demo-api/src/main/java/io/kimmking/rpcfx/demo/api/OrderService.java
@@ -0,0 +1,5 @@
+package io.kimmking.rpcfx.demo.api;
+
+public interface OrderService {
+    Order findOrderById(int id);
+}
---
+++ Week_09/rpc01/rpcfx-demo-api/src/main/java/io/kimmking/rpcfx/demo/api/UserService.java
@@ -0,0 +1,5 @@
+package io.kimmking.rpcfx.demo.api;
+
+public interface UserService {
+    User findById(int id);
+}
